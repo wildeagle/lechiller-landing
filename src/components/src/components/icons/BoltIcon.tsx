f31180052de457d9385be32ebdf2d208dc2e5d96
wildeagle <flavio@ic-agency.com>
2025-11-02 22:33:23 -0400

    Delete src/components/src/components/icons directory

--- src/components/src/components/icons/BoltIcon.tsx
+++
@@ -1,1 +0,0 @@
-import React from 'react'; export const BoltIcon: React.FC = () => (<svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5"><path strokeLinecap="round" strokeLinejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" /></svg>);
